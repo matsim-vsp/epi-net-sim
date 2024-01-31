ebe004d29606fad11ab56b6d9699f16fce98629a
Jakob Rehmann <jakob.rehmann@gmail.com>
2024-01-31 14:45:54 +0100

    copied over code from https://github.com/jakobrehmann/IPSS-Group4

---
+++ src/main/julia/run_locally.jl
@@ -0,0 +1,17 @@
+include("model.jl")
+
+params = Dict(
+    :seeds => 5,
+    :iterations => 200,
+    :base_susceptibilities => [0.1],
+    :recovery_rate => 0.2,
+    :n_nodes => 1000,
+    :network_structures => ["smallworldreg","smallworld"],
+    :local_global_scenarios => ["base"],
+    :mean_degree => 10,
+    :days_until_showing_symptoms => 1,
+    :output_folder => missing
+)
+
+run_model(params)
+
